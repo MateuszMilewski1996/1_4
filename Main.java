246bded5405da5962b0d0d5cda318663bc1fe17e
MateuszMilewsk2 <65113432+MateuszMilewski1996@users.noreply.github.com>
2022-03-15 14:30:10 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,55 @@
+import java.lang.Math;
+import java.util.Scanner;
+public class Main {
+  public static void main(String[] args) {
+    // int a = 3;
+    // int b = 5;
+ Scanner input = new Scanner(System.in);
+    	
+        System.out.print("podaj liczbe a: ");
+        int a = input.nextInt();
+        System.out.println("podales liczbe " + a);
+
+    System.out.print("podaj liczbe b: ");
+        int b = input.nextInt();
+        System.out.println("podales liczbe b " + b);
+    
+    switch(expression) {
+  case 1:
+    int pole_prostokata = a*b;
+         System.out.println("pole prostokata dla podanych wartosi wynosi " + pole_prostokata);
+    break;
+  // case 2:
+  //   int suma_pol_kol = ;
+  //   break;
+
+  // case 3:
+  //   int pole_trojkata_prostokatnego = ;
+  //   break;
+  // case 4:
+  //  int iloraz = ;
+  //   break;
+  default:
+    // code block
+    }
+
+    //     Scanner input = new Scanner(System.in);
+    	
+    //     System.out.print("podaj liczbe a: ");
+    //     int a = input.nextInt();
+    //     System.out.println("podales liczbe " + a);
+
+    // System.out.print("podaj liczbe b: ");
+    //     int b = input.nextInt();
+    //     System.out.println("podales liczbe b " + b);
+    
+       
+   // double pierwiastek_a =(Math.sqrt(a));
+   //  System.out.println("pierwiastek podanej liczby a "+ pierwiastek_a);
+   //  double pierwiastek_b =(Math.sqrt(b));
+   //  System.out.println("pierwiastek podanej liczby b "+ pierwiastek_b);
+   //  double suma = pierwiastek_a + pierwiastek_b;
+   //  System.out.println("suma podanych pierwiastków " + suma);
+   //   // closing the scanner object
+   //       input.close();
+
